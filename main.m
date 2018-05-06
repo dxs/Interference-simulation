b107c56599a91f452b65a70cee846bb1c19b881b
Sven <glaglasven@live.com>
2018-05-06 08:04:26 +0200

    Update pdf

--- main.m
+++ main.m
@@ -5,8 +5,9 @@ sright = Slit(2,1000,10,400);
 
 space = 6;
 
-simpleDoubleSlits(space,5000,5000,sleft,sright);
+%simpleDoubleSlits(space,5000,5000,sleft,sright);
 
 scenter = Slit(40,40,100,14);
 
 %simpleSlits(5000,5000,scenter);
+
